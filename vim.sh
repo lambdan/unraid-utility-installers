2354e9beb562fa5e37beae2e345e5e2949921f60
David Silverlind <lambdan@icloud.com>
2026-03-29 13:13:04 +0200

    vim script

---
+++ vim.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+URL="https://github.com/lxhillwind/vim-bin/releases/download/v9.2.0081/vim-v9.2.0081-linux-x64.tar.xz"
+DIR="/tmp/.vim"
+DEST="/usr/local/bin/vim"
+DOWNLOAD_DEST="$DIR/vim.tar.xz"
+
+echo "### vim install script ###"
+echo "URL: $URL"
+echo "DIR: $DIR"
+echo "Symlink targets: $DEST"
+echo "#############################"
+
+mkdir -p "$DIR"
+echo "Downloading $URL"
+curl -L -o "$DOWNLOAD_DEST" "$URL"
+echo "Untarring..."
+tar xf "$DOWNLOAD_DEST" -C "$DIR" --strip-components=1
+echo "Symlinking..."
+ln -s "$DIR/AppRun" "$DEST"
+
+# verify
+file -s "$(which vim)"
+
+echo "#####"
+echo "Done!"
+echo "#####"
